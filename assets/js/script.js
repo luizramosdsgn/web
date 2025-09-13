b23202cdec6fcc6a8decf926fdf65623da414ca5
Luiz <henrique10v20@gmail.com>
2025-09-12 22:18:07 -0300

    Melhorias gerais no funcionamento

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,15 +5,49 @@ const iconSun = document.querySelector('.iconSun');
 
 iconMoon.addEventListener('click', () => {
 
-    iconMoon.classList.add('inativo');
-    iconSun.classList.remove('inativo');
+  iconMoon.classList.add('inativo');
+  iconSun.classList.remove('inativo');
 });
 
 iconSun.addEventListener('click', () => {
-  
-    iconSun.classList.add('inativo');
-    iconMoon.classList.remove('inativo');
+
+  iconSun.classList.add('inativo');
+  iconMoon.classList.remove('inativo');
 });
+
+
+class MobileNavbar {
+  constructor() {
+    this.mobileMenu = document.querySelector('.menu-mobile')
+    this.navList = document.querySelector('.menu-padrao')
+    this.navLinks = document.querySelectorAll('.menu-li')
+    this.linkInsta = document.querySelector('.link-insta')
+    this.nav = document.querySelector('nav')
+    this.activeClass = "active"
+  }
+
+  handleClick() {
+    this.navList.classList.toggle(this.activeClass);
+    this.mobileMenu.classList.toggle(this.activeClass);
+    this.linkInsta.classList.toggle(this.activeClass);
+    this.nav.classList.toggle(this.activeClass);
+  }
+
+  addClickEvent() {
+    this.mobileMenu.addEventListener("click", () => this.handleClick())
+  }
+
+  init() {
+    if (this.mobileMenu) {
+      this.addClickEvent()
+    }
+    return this
+  }
+}
+
+const mobileNavbar = new MobileNavbar();
+
+mobileNavbar.init();
 
 
 // Hover nos cards de serviço
